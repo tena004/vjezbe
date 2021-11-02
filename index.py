e7e7cda6a043afa7f9359bebc84347cdec544ac1
tena004 <tena.zivkovic15@gmail.com>
2021-11-02 17:13:47 +0100

    treci commit

--- index.py
+++ index.py
@@ -1,1 +1,14 @@
-print("Hello world!")
+print("Dobrodošli u Unidu sustav!")
+print("Za prijavu upišite broj 1, za registraciju broj 2:")
+odabir = int(input("Unesite broj: "))
+
+while odabir != 1 and odabir != 2:
+    odabir = int(input("Unesite broj: "))
+
+
+if odabir == 1:
+    print("Dobrodošli u prijavu!")
+
+else:
+    print("Dobrodošli u registraciju!")
+    
