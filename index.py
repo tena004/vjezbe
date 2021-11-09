5ecae00c5b797c09316d4f4a9084a959d6d81ce3
Tena <tena.zivkovic15@gmail.com>
2021-11-09 17:33:27 +0100

    gotov login

--- index.py
+++ index.py
@@ -1,3 +1,16 @@
+import sqlite3
+from datetime import date
+
+def register_user(name, email, password, contact):
+    today = date.today()
+    con = sqlite3.connect('baza.db')
+    cur = con.cursor()
+    query = cur.execute("INSERT INTO user (name, email, password, contact, created_at) VALUES (?, ?, ?, ?, ?)", (name, email, password, contact, today))
+    con.commit()
+    con.close()
+
+
+
 print("Dobrodošli u Unidu sustav!")
 print("Za prijavu upišite broj 1, za registraciju broj 2:")
 odabir = int(input("Unesite broj: "))
@@ -11,4 +24,15 @@ if odabir == 1:
 
 else:
     print("Dobrodošli u registraciju!")
+    name = input("Unesite Vaše ime: ")
+    email = input("Unesite e-mail: ")
+    pwd = input("Unesite lozinku: ")
+    contact = input("Unesite kontakt broj: ")
+    register_user(name, email, pwd, contact)
+    
+
+
+
+
+    
     
