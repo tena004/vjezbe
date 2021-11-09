c64659d05685c8270d93c44d50d885b53451cea5
Tena <tena.zivkovic15@gmail.com>
2021-11-09 16:53:24 +0100

    cetvrti commit

---
+++ database.py
@@ -0,0 +1,13 @@
+import sqlite3
+con = sqlite3.connect('baza.db')
+
+cur = con.cursor()
+
+cur.execute('''CREATE TABLE IF NOT EXISTS user
+               (id integer PRIMARY KEY,
+               name text NOT NULL,
+               email text NOT NULL,
+               password text NOT NULL,
+               contact text NOT NULL,
+               created_at text NOT NULL)''')
+
