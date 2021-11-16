134b17c15954d318c339a8ee3399fa71bbda702e
Ivan <isusnjara7@gmail.com>
2021-11-16 17:08:30 +0100

    dodan hash i login

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import sqlite3
+import hashlib
 from datetime import date
 
 def register_user(name, email, password, contact):
@@ -9,7 +10,24 @@ def register_user(name, email, password, contact):
     con.commit()
     con.close()
 
+def login():
+    email = input("Unesite e-mail: ")
+    pwd = input("Unesite lozinku: ")
+    pwd = hashing(pwd)
+    con = sqlite3.connect('baza.db')
+    cur = con.cursor()
+    for row in cur.execute('SELECT * FROM user'):
+        rowEmail =  row[2]
+        rowPwd = row[3]
+        if(email == rowEmail and pwd == rowPwd):
+            print('Uspješna prijava!')
+            return
+    print('Krivi email ili password!')
+    return
 
+def hashing(pwd):
+    result = hashlib.sha256(pwd.encode('utf-8')).hexdigest()
+    return result
 
 print("Dobrodošli u Unidu sustav!")
 print("Za prijavu upišite broj 1, za registraciju broj 2:")
@@ -21,6 +39,7 @@ while odabir != 1 and odabir != 2:
 
 if odabir == 1:
     print("Dobrodošli u prijavu!")
+    login()
 
 else:
     print("Dobrodošli u registraciju!")
@@ -28,7 +47,7 @@ else:
     email = input("Unesite e-mail: ")
     pwd = input("Unesite lozinku: ")
     contact = input("Unesite kontakt broj: ")
-    register_user(name, email, pwd, contact)
+    register_user(name, email, hashing(pwd), contact)
     
 
 
